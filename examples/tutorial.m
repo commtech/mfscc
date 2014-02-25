8fa15007585434d5c793ac0d9fdd4f2e18314143
Josh Droney <joshd@commtech-fastcom.com>
2014-02-25 15:00:45 -0600

    Added a purge at the end of the tutorial example, just for safety.

--- examples/tutorial.m
+++ examples/tutorial.m
@@ -1,7 +1,7 @@
 fscc = mfscc();
 p = fscc.connect(0);
-written = fscc.write_with_blocking(p, 'Hello world!', 12);
+written = fscc.write_with_blocking(p, 'Hello world.', 12);
 [message, message_size] = fscc.read_with_timeout(p, 5);
-char(message.Value)
-disp(message_size.Value)
+display(message)
+fscc.purge(p,1,1);
 fscc.disconnect(p);
